defe303ec9c71ecbe6c2dc197bbcdc8870fc5541
GuiMac <guibasconti@gmail.com>
2017-12-16 09:18:27 -0200

    Add constants

---
+++ server/utils/constants.js
@@ -0,0 +1,15 @@
+/**
+ * All project constants
+ * @module utils/constants
+*/
+module.exports = {
+  messages: {
+    error: {
+      INVALID_CRYPTO_CURRENCY: 'You need to send a valid crypto currency.',
+      INVALID_CURRENCY: 'You need to send a valid currency.',
+    },
+    success: {
+      USER_REGISTERED: 'User registered.'
+    }
+  }
+};
