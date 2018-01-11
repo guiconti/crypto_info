baf0a766bdc044d563a1034bbf250a0b89cc47cd
GuiMac <guibasconti@gmail.com>
2018-01-11 00:52:59 -0200

    Create integer validation

--- server/utils/validator.js
+++ server/utils/validator.js
@@ -10,7 +10,18 @@ const _ = require('underscore');
  *
  * @param {string} stringToValidate - String to be validated
  * @return {boolean} - True case the string is valid and false if it is not
- */
+*/
 exports.isValidString = stringToValidate => {
   return _.isString(stringToValidate) && stringToValidate.trim().length > 0;
+};
+
+/**
+ * Validate if the input is a valid integer
+ * @param {string} integerToValidate - Integer to be validated
+ * @return {boolean} - True case the integer is valid and false if it is not
+*/
+exports.isValidInteger = integerToValidate => {
+  return constants.regex.integer.test(integerToValidate) 
+    && parseInt(integerToValidate) <= Number.MAX_SAFE_INTEGER 
+    && parseInt(integerToValidate) >= Number.MIN_SAFE_INTEGER;
 };
