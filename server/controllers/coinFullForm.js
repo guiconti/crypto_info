d3cc5b7fdfcae3efd47bfca84d20c7070d810f33
leonardo a <leonardo.akato@gmail.com>
2018-01-16 12:02:01 -0200

    added get coin full form name

--- server/controllers/coinFullForm.js
+++ server/controllers/coinFullForm.js
@@ -6,6 +6,7 @@
 const Kucoin = require('../utils/Kucoin');
 const validator = require('../utils/validator');
 const constants = require('../utils/constants');
+const request = require('request');
 
 /**
  * Get information about one coin at user`s wallet
@@ -17,11 +18,62 @@ const constants = require('../utils/constants');
 */
 module.exports = (req, res) => {
   //let {coin} = req.params;
-  console.log('ello');
+  let {coin} = req.body;
+  let url = constants.urls.KUCOIN_PREFIX + constants.urls.KUCOIN_GET_COIN_INFO;
+  console.log(url);
+  //Kucoin.getCoinInfo;
   
-  return res.status(400).json({
-    data: 'batuta esse postman  eim'
+  request.get({url: url}, (err, httpResponse, body) => {
+    let databody = JSON.parse(body);
+    let ans = 'moeda nawa: ';
+    if (err)
+    return res.status(500).json({
+      msg: constants.messages.error.OVERBUFF_API
+    });
+    
+    console.log (databody.code);
+    console.log('coin: ' + coin.toUpperCase());
+    
+    if(coin.toUpperCase() == "DBC")
+      return res.status(200).json({
+        msg: ans += "DeepBrain Chan"
+      });
+    databody.data.forEach(datacoin => {
+      if (datacoin.coin == coin.toUpperCase()){
+        ans += datacoin.name;
+      }
+    });
+    return res.status(200).json({
+      msg: ans
+    });
+    // overbuffApiDecriptor(body)
+    //   .then((heroesInfo) => {
+    //     heroesInfo.sort((a, b) => {
+    //       return a.pick_rate>=b.pick_rate?-1:1;
+    //     });
+    //     let topPickedHeroes = heroesInfo.slice(0, constants.overbuff.NUMBER_OF_TOP_PICKED);
+    //     topPickedHeroes.forEach((heroInfo) => {
+    //       for (key in heroInfo){
+    //         if (!constants.overbuff.PICK_INFOS.includes(key)){
+    //           delete heroInfo[key];
+    //         }
+    //       }
+    //       heroInfo.hero = constants.overbuff.heroes[heroInfo.hero];
+    //       heroInfo.pick_rate = Math.round(heroInfo.pick_rate * 10000)/100 + '%';
+    //     });
+    //     return res.status(200).json({
+    //       msg: topPickedHeroes
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     logger.error(err);
+    //     return res.status(500).json({
+    //       msg: constants.messages.error.OVERBUFF_API
+    //     });
+    //   });
   });
+
+
   // if (!validator.isValidString(coin))
   //   return res.status(400).json({
   //     data: constants.messages.error.INVALID_CURRENCY
--- server/core/router.js
+++ server/core/router.js
@@ -12,16 +12,22 @@ const registerWallet = require('../controllers/registerWallet');
 const cryptoInfo = require('../controllers/cryptoInfo');
 const walletBalance = require('../controllers/walletBalance');
 const walletCoinBalance = require('../controllers/walletCoinBalance');
-
 const generateApiKey = require('../controllers/generateApiKey');
+const coinFullForm = require('../controllers/coinFullForm');
 
 //  Internal APIs
 router.post('/wallet/register', registerWallet);
+
+//  Placeholder API
+router.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Hi!' });
+});
 
 //  Kucoin APIs (TODO: Make it accept any coded exchange)
 router.get('/convert_currency', cryptoInfo);
 router.get('/:userId/wallet/balance', walletBalance);
 router.get('/:userId/wallet/:coin/balance', walletCoinBalance);
 //router.post('/create_api_key', generateApiKey);
+router.post('/coin_full_form', coinFullForm);
 
 module.exports = router;
