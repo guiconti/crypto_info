37854b78c155ffb4c9434ba3f380659dabd84333
leonardo a <leonardo.akato@gmail.com>
2018-01-16 17:33:49 -0200

    minor code optimization changes

--- server/controllers/coinFullForm.js
+++ server/controllers/coinFullForm.js
@@ -16,36 +16,35 @@ const request = require('request');
  * @throws {object} - Returns a msg that indicates a fail
  * 
 */
-module.exports = (req, res) => {
+module.exports = (req, res) => { 
   //let {coin} = req.params;
   let {coin} = req.body;
   let url = constants.urls.KUCOIN_PREFIX + constants.urls.KUCOIN_GET_COIN_INFO;
-  console.log(url);
   //Kucoin.getCoinInfo;
   
   request.get({url: url}, (err, httpResponse, body) => {
     let databody = JSON.parse(body);
-    let ans = 'moeda nawa: ';
+    let answ = 'moeda nawa: ';
     if (err)
     return res.status(500).json({
-      msg: constants.messages.error.OVERBUFF_API
+      msg: constants.messages.error.UNEXPECTED
     });
-    
-    //remove this crap below:
-    console.log (databody.code);
-    console.log('coin: ' + coin.toUpperCase());
     
     if(coin.toUpperCase() == "DBC")
       return res.status(200).json({
-        msg: ans += "DeepBrain Chan"
+        msg: answ += "DeepBrain Chan"
       });
     databody.data.forEach(datacoin => {
       if (datacoin.coin == coin.toUpperCase()){
-        ans += datacoin.name;
+        //answ += datacoin.name;
+        return res.status(200).json({
+          msg: answ + datacoin.name
+        });
       }
     });
-    return res.status(200).json({
-      msg: ans
+    return res.status(400).json({
+      msg: 'num achô'
     });
+
   });
 };
